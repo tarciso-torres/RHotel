d2570b17ca3d9e8d5702ac1891eda0770d4f6817
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-07 22:55:31 -0300

    Create project

---
+++ src/main/java/com/redfort/rhotel/RHotelApplication.java
@@ -0,0 +1,13 @@
+package com.redfort.rhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RHotelApplication.class, args);
+	}
+
+}
