5703c9417810d472b5dffb3bcd0979efa89ae748
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-17 22:30:32 -0300

    Create ApiExceptionHandler

--- src/main/java/com/redfort/rhotel/error/ApiExceptionHandler.java
+++ src/main/java/com/redfort/rhotel/error/ApiExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.redfort.rhotel.error;
 
-import java.util.Collection;
-import java.util.Collections;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -18,43 +19,51 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.redfort.rhotel.error.ErrorResponse.ApiError;
+
 import lombok.RequiredArgsConstructor;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ApiExceptionHandler {
-	
-	private static final String NO_MESSAGE_AVAILABLE = "No message available";
-	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+	private static final String NO_MESSSAGE_AVAILABLE = "No message available";
+	private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
 	private final MessageSource apiErrorMessageSource;
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> handleNotValidException(MethodArgumentNotValidException exception,
 			Locale locale) {
-		
 		Stream<ObjectError> errors = exception.getBindingResult().getAllErrors().stream();
-		
-		List<ErrorResponse.ApiError> apiErrors = errors
-				.map(ObjectError::getDefaultMessage)
-				.map(code -> toApiError(code, locale))
-				.collect(Collectors.toList());
-		
-		ErrorResponse errorResponse = ErrorResponse.od(HttpStatus.BAD_REQUEST, apiErrors);
+
+		List<ApiError> apiErrors = errors.map(ObjectError::getDefaultMessage).map(code -> toApiError(code, locale))
+				.collect(toList());
+
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, apiErrors);
 		return ResponseEntity.badRequest().body(errorResponse);
-		
-	}
-	
-	private ErrorResponse.ApiError toApiError(String code, Locale locale){
-		String message;
-		try {
-			message = apiErrorMessageSource.getMessage(code, null, locale);
-			LOG.error("Could not find any message for {} code under {} locale", code, locale);
-		}catch (NoSuchMessageException e) {
-			message = NO_MESSAGE_AVAILABLE;
-		}
-		
-		return new ErrorResponse.ApiError(code, message);
 	}
 
+	@ExceptionHandler(InvalidFormatException.class)
+	public ResponseEntity<ErrorResponse> handleInvalidFormatException(InvalidFormatException exception, Locale locale) {
+		final String errorCode = "generic-1";
+		final HttpStatus status = HttpStatus.BAD_REQUEST;
+		final ErrorResponse errorResponse = ErrorResponse.of(status,
+				toApiError(errorCode, locale, exception.getValue()));
+		return ResponseEntity.badRequest().body(errorResponse);
+	}
+
+	public ApiError toApiError(String code, Locale locale, Object... args) {
+		String message;
+		try {
+			message = apiErrorMessageSource.getMessage(code, args, locale);
+		} catch (NoSuchMessageException e) {
+			LOG.error("Could not find any message for {} code under {} locale", code, locale);
+			message = NO_MESSSAGE_AVAILABLE;
+		}
+
+		return new ApiError(code, message);
+	}
 }
