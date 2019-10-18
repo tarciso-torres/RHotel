252b28ff385f4da029b312860b13a2b88917793a
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-17 22:30:57 -0300

    Create ErrorResponse

--- src/main/java/com/redfort/rhotel/error/ErrorResponse.java
+++ src/main/java/com/redfort/rhotel/error/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.redfort.rhotel.error;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static lombok.AccessLevel.PRIVATE;
 
 import java.util.Collections;
 import java.util.List;
@@ -9,28 +10,28 @@ import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
-import lombok.RequiredArgsConstructor;;
+import lombok.RequiredArgsConstructor;
 
 @JsonAutoDetect(fieldVisibility = ANY)
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = PRIVATE)
 public class ErrorResponse {
-	
-	private final int statusCode;
-	private final List<ApiError> errors;
-	
-	static ErrorResponse of(HttpStatus status, List<ApiError> errors) {
-		return new ErrorResponse(status.value(), errors);
-	}
-	
-	static ErrorResponse of(HttpStatus status, ApiError error) {
-		return of(status, Collections.singletonList(error));
-	}
-	
-	@JsonAutoDetect(fieldVisibility = ANY)
-	@RequiredArgsConstructor
-	static class ApiError {
-		private final String code;
-		private final String message;
-	}
+
+    private final int statusCode;
+    private final List<ApiError> errors;
+
+    static ErrorResponse of(HttpStatus status, List<ApiError> errors) {
+        return new ErrorResponse(status.value(), errors);
+    }
+
+    static ErrorResponse of(HttpStatus status, ApiError error) {
+        return of(status, Collections.singletonList(error));
+    }
+
+    @JsonAutoDetect(fieldVisibility = ANY)
+    @RequiredArgsConstructor
+    static class ApiError {
+        private final String code;
+        private final String message;
+    }
 
 }
